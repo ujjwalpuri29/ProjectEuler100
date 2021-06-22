429ebbb0316c29738c6195cb379a230d8f3682ad
Ujjwal Puri <ujjwalpuri29@yahoo.com>
2021-06-22 20:29:20 +0530

    Create 006.js

---
+++ 006.js
@@ -0,0 +1,8 @@
+function sumSquareDifference(n) {
+    //Using series formulae
+    var sumOfSquares = n*(n+1)*(2*n+1)/6;
+    var squareOfSum = Math.pow(n*(n+1)/2, 2);
+    return squareOfSum - sumOfSquares;
+}
+
+sumSquareDifference(100);
