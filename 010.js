606ac5addd5223805e1f69d8f6c611ad16ac4b9e
Ujjwal Puri <ujjwalpuri29@yahoo.com>
2021-06-23 23:30:37 +0530

    Add prime-number sieve method

--- 010.js
+++ 010.js
@@ -1,6 +1,7 @@
 // Problem 10: Summation of primes
 
-function primeSummation(n) {
+//Method 1: Divisibility check
+function primeSummation(n) {    
     function isPrime(num) {
         for (let i=2; i<=Math.ceil(Math.sqrt(num)); i++)
             if (num%i==0)
@@ -16,4 +17,28 @@ function primeSummation(n) {
     return sumOfPrimes;
 }
 
+//Method 2: Prime number sieve
+function primeSummation2(n) {
+    let isPrime = Array(n).fill(true); //initialize array of n elements and fill with true
+    
+    //0 and 1 are not prime 
+    isPrime[0] = false;
+    isPrime[1] = false; 
+    
+    //change multiples of each prime number to false since they are not false
+    for (let i=2; i<=Math.sqrt(n); i++)
+        if (isPrime[i])
+            for (let j=i**2; j<n; j+=i)
+                isPrime[j] = false; 
+
+    let sum = 0;
+    for (let i=0; i<n; i++)
+        if (isPrime[i])
+            sum += i;
+
+    return sum;
+    //return isPrime.reduce((sum, prime, index) => prime ? sum + index : sum, 0;
+}
+
 console.log(primeSummation(2001));
+console.log(primeSummation2(2001));
