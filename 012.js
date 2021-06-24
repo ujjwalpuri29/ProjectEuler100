cbeb88324c8380047115549783c64bea5029debf
Ujjwal Puri <ujjwalpuri29@yahoo.com>
2021-06-24 23:22:00 +0530

    Add sieve method

--- 012.js
+++ 012.js
@@ -1,7 +1,7 @@
 // Problem 12: Highly divisible triangular number
 
-//Brute-force solution
-//Not hightly efficient. Does not work for n more than 48
+//Method 1: Brute-force solution
+//Not hightly efficient. Does not work for large n
 function isPrime(n) {
     for (let i=2; i<=Math.sqrt(n); i++)
         if (n%i==0)
@@ -46,5 +46,58 @@ function divisibleTriangleNumber(n) {
     return triangle;
 }
 
-//Parameter max-limit is 48
 console.log(divisibleTriangleNumber(23));
+
+//-----------------------------------------------//
+
+//Method 2: Sieve of Eratosthenes
+//Faster method
+const MAX = 100000;
+const prime = new Array(MAX+1).fill(true);
+
+function sieve() {
+    prime[0] = false;
+    prime[1] = false;
+
+    var limit = Math.ceil(Math.sqrt(MAX));
+    for (let i=2; i<=limit; i++)
+        if (prime[i])
+            for (let j=i*i; j<MAX; j+=i)
+                prime[j] = false;
+}
+
+function divCount(n) {
+    var divisors = 1;
+    for (let i=2; i<=n; i++)
+    {
+        if (prime[i])
+        {
+            var exponent = 0;
+            if (n%i==0)
+            {
+                while (n%i==0)
+                {
+                    n /= i;
+                    exponent++;
+                }
+                divisors *= (exponent+1);
+            }
+        }
+    }
+    return divisors;
+}
+
+function divisibleTriangleNumberSieve(n) {
+  let i = 1, a = 0;
+  while (a < n)
+  {
+      var triangle = i*(i+1)/2;
+      a = divCount(triangle);
+      i++;
+  }
+  return triangle;
+}
+
+//First initialize the sieve array then call for divisors
+sieve();
+console.log(divisibleTriangleNumberSieve(500));
