1e9c5d25a7abb3097ccb94d328ab0e342eb4e70c
Ujjwal Puri <ujjwalpuri29@yahoo.com>
2021-06-29 21:22:50 +0530

    Fix indent

--- 018.js
+++ 018.js
@@ -4,14 +4,14 @@
 //Returning the topmost element since it has the maximum sum of the elements underneath
 function maximumPathSumI(triangle) {
     for (let i = triangle.length - 2; i >= 0; i--)
-      for (let j = 0; j <= i; j++)
-        triangle[i][j] += ((triangle[i+1][j] > triangle[i+1][j+1]) ? triangle[i+1][j] : triangle[i+1][j+1]);
+      	for (let j = 0; j <= i; j++)
+        	triangle[i][j] += ((triangle[i+1][j] > triangle[i+1][j+1]) ? triangle[i+1][j] : triangle[i+1][j+1]);
     return triangle[0][0];
-  }
+}
   
-  const testTriangle = [[3, 0, 0, 0],
-                        [7, 4, 0, 0],
-                        [2, 4, 6, 0],
-                        [8, 5, 9, 3]];
+const testTriangle = [[3, 0, 0, 0],
+                      [7, 4, 0, 0],
+                      [2, 4, 6, 0],
+                      [8, 5, 9, 3]];
   
-  console.log(maximumPathSumI(testTriangle));
+console.log(maximumPathSumI(testTriangle));
